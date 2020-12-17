bd6090ff7442e2b7ad9ab7aef8696d5d7c7d1593
liuxinhongg <644391351@qq.com>
2020-12-17 16:39:03 +0800

    async

--- es6/dist/spread.js
+++ es6/dist/spread.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
@@ -18,7 +18,54 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 {
 	// instanceof
 	var arr = [1, 2, 3, 4, 5];
-	console.log(typeof arr === "undefined" ? "undefined" : _typeof(arr)); //object 
+	console.log(typeof arr === 'undefined' ? 'undefined' : _typeof(arr)); //object 
 	console.log(arr instanceof Array); //true
 	console.log(Array.isArray(arr)); //true
+}
+{
+	var _arr = [11, 22, 33];
+	console.log(_arr);
+	console.log(_arr.constructor);
+	console.log(Array);
+	console.log(_arr.constructor === Array); //true
+	var _obj = { name: 'lxh' };
+	console.log(_obj);
+	console.log(_obj.toString()); //[object Array]
+	console.log(_arr.toString());
+	// 想用数组调用object的tostring方法
+	console.log(Object.prototype.toString.call(_arr)); //[object Array] call改变了this的指向
+	// 验证
+	console.log(Object.prototype.toString.call(_arr) === '[object Array]');
+	// console.log(obj.toString.call(arr));
+}
+{
+	var _fn = function _fn() {
+		"use strict";
+
+		console.log(this);
+	};
+
+	_fn();
+}
+{
+	var data = new Date();
+	console.log(data); //Thu Dec 17 2020 15:48:51 GMT+0800 (中国标准时间)
+	console.log(data.toJSON()); //可读性的日期
+}
+{
+	var fun = function fun() {
+		console.log(this);
+	};
+
+	var obj = { name: "xiaomei" };
+
+	fun.call(obj);
+	fun.apply(obj);
+	var bind1 = fun.bind(obj);
+	bind1();
+}
+{
+	"use strict";
+	a = 10;
+	console.log(a);
 }
--- es6/src/spread.js
+++ es6/src/spread.js
@@ -17,3 +17,51 @@
 	console.log(arr instanceof Array);//true
 	console.log(Array.isArray(arr))//true
 } 
+{
+	let arr = [11,22,33];
+	console.log(arr);
+	console.log(arr.constructor);
+	console.log(Array)
+	console.log(arr.constructor===Array);//true
+	let obj = { name:'lxh' }
+	console.log(obj)
+	console.log(obj.toString())//[object Array]
+	console.log(arr.toString())
+	// 想用数组调用object的tostring方法
+	console.log(Object.prototype.toString.call(arr))//[object Array] call改变了this的指向
+	// 验证
+	console.log(Object.prototype.toString.call(arr)==='[object Array]')
+	// console.log(obj.toString.call(arr));
+}
+{
+	function fn(){
+		"use strict"
+		console.log(this);
+	}
+	fn()
+}
+{
+	var data = new Date();
+	console.log(data);//Thu Dec 17 2020 15:48:51 GMT+0800 (中国标准时间)
+	console.log(data.toJSON())//可读性的日期
+	
+}
+{
+	var obj = {name:"xiaomei"};
+	function fun(){
+		console.log(this)
+	}
+	fun.call(obj)
+	fun.apply(obj)
+	var bind1= fun.bind(obj);
+	bind1()
+}
+{
+	"use strict"
+	a = 10;
+	console.log(a);
+	function fun(name,name){
+		console.log(name)
+	}
+	fun("我是谁？","我在那")
+}
