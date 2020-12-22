c5bffbba903f6c6a6dc2d8843aa053c32a5908b5
liuxinhongg <644391351@qq.com>
2020-12-22 17:13:42 +0800

    tijiao

--- nodeJs/twoday/03server和mysql.js
+++ nodeJs/twoday/03server和mysql.js
@@ -1,10 +1,17 @@
 const http = require("http");
 const my = require("./ys.js");
+let url = require("url");
 const server = http.createServer((req,res)=>{
 	res.setHeader("content-type","text/html;charset=utf-8");
-	my.query("select * from user",[],(result)=>{
+	res.setHeader("Access-Control-Allow-Origin","*");//解决跨域
+	let aa = url.parse(req.url,true).query;
+	console.log(aa);
+	// res.end(1)
+	let sqladd = `insert into user(username,password,email,phone,nick) values (?,?,?,?,?)`;
+	let sqlparams = [aa.username,aa.password,aa.email,aa.phone,aa.nick];
+	my.query(sqladd,sqlparams,(result)=>{
 		// argument must be of type string or an instance of Buffer. Received an instance of Array
-		res.end(JSON.stringify(result))
+		res.end("success~")
 	})
 })
 server.listen(3000);
